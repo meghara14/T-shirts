c2db7f25bc5ee4f0a84db6066a6f35d2d802bdc5
Magdalena Lesisz <meghara@icloud.com>
2023-10-09 13:34:41 +0200

    last changes

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -2,6 +2,7 @@ import styles from './ProductForm.module.scss';
 import OptionSize from '../OptionSize/OptionSize';
 import OptionColor from '../OptionColor/OptionColor';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
 
 const ProductForm = (props) => {
@@ -23,15 +24,26 @@ const ProductForm = (props) => {
             <h2 className={styles.name}>{props.title}</h2>
             <span className={styles.price}>Price: {props.price}$</span>
         </header>
-        <form onSubmit={(event) => sentOrder(event, props.title, props.price, props.currentSize, props.currentColor)}>
+        <form className={styles.buttonContainer} onSubmit={(event) => sentOrder(event, props.title, props.price, props.currentSize, props.currentColor)}>
                 <OptionSize sizes={props.sizes} currentSize={props.currentSize} setCurrentSize={props.setCurrentSize}  />
                 <OptionColor colors={props.colors} currentColor={props.currentColor} setCurrentColor={props.setCurrentColor} />
-                <Button>
+                <Button className={styles.button}>
                     <span className="fa fa-shopping-cart" />
                 </Button>
         </form>
         </div>
     )
 }
+
+ProductForm.propTypes = {
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    sizes: PropTypes.array.isRequired,
+    currentSize: PropTypes.object.isRequired,
+    colors: PropTypes.array.isRequired,
+    currentColor: PropTypes.string.isRequired,
+    setCurrentSize: PropTypes.func.isRequired,
+    setCurrentColor: PropTypes.func.isRequired,
+};
 
 export default ProductForm
